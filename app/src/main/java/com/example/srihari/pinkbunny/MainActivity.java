058673e3cf2d17932f1df98a775077ccff769f4e
pythongiant <srihari.unnikrishnan@gmail.com>
2017-10-20 23:43:33 +0530

    added a background. added some stuff. dunno why this took me an hour.

--- app/src/main/java/com/example/srihari/pinkbunny/MainActivity.java
+++ app/src/main/java/com/example/srihari/pinkbunny/MainActivity.java
@@ -9,27 +9,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mHeading;
+    private TextView mDayName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
+        //find the view
         mHeading=(TextView) findViewById(R.id.Heading);
-
-
+        //get the Font
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/CookieMonster.ttf");
-
         mHeading.setTypeface(custom_font);
+        //set text for the heading
+        mHeading.setText("\n\nWelcome to Pink Bunny");
 
-        mHeading.setText("Welcome to Pink Bunny");
-
-
+        //find the day name
+        mDayName= (TextView) findViewById(R.id.dayName);
+        mDayName.setText("\n\n\n\nDay 1:");
     }
 }
 
---
+++ app/src/main/java/com/example/srihari/pinkbunny/ObjectiveNames.java
@@ -0,0 +1,12 @@
+package com.example.srihari.pinkbunny;
+
+/**
+ * Created by srihari on 20/10/17.
+ */
+
+public class ObjectiveNames {
+    String[] obejctiveName={"Be happy!"};
+    public String AllNames(int listNum){
+        return obejctiveName[listNum];
+    }
+}
